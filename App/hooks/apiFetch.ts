17ba1013648150987e0282a3eabe4a9f6d6712c6
Felipe Borba <felipe.borba0109@gmail.com>
2022-07-09 23:20:53 -0300

    log error

--- App/hooks/apiFetch.ts
+++ App/hooks/apiFetch.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosResponse } from "axios";
+import axios from "axios";
 
 axios.defaults.baseURL = "https://test-api.atom6studio.com";
 
@@ -26,21 +26,23 @@ export namespace authApi {
     password: string;
   };
   export async function signUp(body: SignUpRequest) {
-    return await axios.post<AuthResponse | ErrorResponse>("/auth/signup", body);
+    return axios
+      .post<AuthResponse>("/auth/signup", body)
+      .then((response) => response.data)
+      .catch(requestError());
   }
 
   export type SignInRequest = { username: string; password: string };
   export async function signIn(body: SignInRequest) {
-    const response = await axios.post<AuthResponse | ErrorResponse>(
-      "/auth/signin",
-      body
-    );
+    const response = await axios
+      .post<AuthResponse>("/auth/signin", body)
+      .then((response) => response.data)
+      .catch(requestError());
 
-    if (response.status == 200) {
-      const data = response.data as AuthResponse;
+    if (response) {
       axios.defaults.headers.common[
         "Authorization"
-      ] = `Bearer ${data.access.token}`;
+      ] = `Bearer ${response.access.token}`;
     }
 
     return response;
@@ -48,16 +50,46 @@ export namespace authApi {
 
   // TODO add response type
   export async function refresh(refreshToken: string) {
-    return axios.post("/auth/refresh", {
-      refresh_token: refreshToken,
-    });
+    return axios
+      .post("/auth/refresh", {
+        refresh_token: refreshToken,
+      })
+      .then((response) => response.data)
+      .catch(requestError());
   }
 
   export async function check() {
-    return axios.get<UserDto | ErrorResponse>("/auth/check");
+    return axios
+      .get<UserDto>("/auth/check")
+      .then((response) => response.data)
+      .catch(requestError());
   }
 
   export async function remove(id: string) {
-    return axios.delete<number | ErrorResponse>(`/auth/delete/${id}`);
+    return axios
+      .delete<number>(`/auth/delete/${id}`)
+      .then((response) => response.data)
+      .catch(requestError());
   }
+}
+
+function requestError() {
+  return (error: any) => {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log(error.response.data);
+      console.log(error.response.status);
+      alert(error.response.data.message)
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Error", error.message);
+    }
+    console.log(error.config);
+  };
 }
